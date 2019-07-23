c411296529a446124959b6491d7f5ab671492c70
brady sisk <bradysiskpro@yahoo.com>
2019-07-23 19:41:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,30 @@
 def using_include(array, element)
-
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.include?("wow")
+  array.include?("tardis")
 end
 
 def using_sort(array)
-
+ rams = ["goff", "gurley", "donlad", "suh"]
+ rams.sort
 end
 
 def using_reverse(array)
-
+ rams = ["goff", "gurley", "donlad", "suh"]
+ rams.reverse
 end
 
 def using_first(array)
-
+ rams = ["goff", "gurley", "donlad", "suh"]
+ rams.first
 end
 
 def using_last(array)
-
+ rams = ["goff", "gurley", "donlad", "suh"]
+ rams.last
 end
 
 def using_size(array)
-
+ rams = ["goff", "gurley", "donlad", "suh","mcdonald","anderson"]
+rams.size
 end
